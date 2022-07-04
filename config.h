98211df786252e49b181004388b754610cde685c
Matt Allwright <matt.a.4890@gmail.com>
2022-07-04 17:55:02 -0600

    Add basic volume and time

--- config.h
+++ config.h
@@ -1,16 +1,8 @@
 #define CMDLENGTH 45
-#define DELIMITER "  "
+#define DELIMITER " | "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("amixer sget Master | awk -F\"[][]\" '/Left:/ { print $2 }'", 0, 1), // Volume
+	BLOCK("date", 1, 2)                                                        // Time & Date
 };
