3b64a349dcbcbe08adc8d042af8929da451fa4a0
Matt Allwright <matt.a.4890@gmail.com>
2022-07-04 18:11:52 -0600

    Fix weird rectangle char

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 #define CMDLENGTH 45
 #define DELIMITER " | "
-#define CLICKABLE_BLOCKS
+// #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
 	BLOCK("amixer sget Master | awk -F\"[][]\" '/Left:/ { print $2 }'", 0, 1), // Volume
-	BLOCK("date", 1, 2)                                                        // Time & Date
+	BLOCK("date", 1, 0)                                                        // Time & Date
 };
