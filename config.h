385742b8880b93916d961f7b59995accae464bc3
Matt Allwright <matt.a.4890@gmail.com>
2022-07-04 18:15:31 -0600

    Hacky space

--- config.h
+++ config.h
@@ -3,6 +3,6 @@
 // #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("amixer sget Master | awk -F\"[][]\" '/Left:/ { print $2 }'", 0, 1), // Volume
+	BLOCK("echo \" $(amixer sget Master | awk -F\"[][]\" '/Left:/ { print $2 }')\"", 0, 1), // Volume
 	BLOCK("date", 1, 0)                                                        // Time & Date
 };
